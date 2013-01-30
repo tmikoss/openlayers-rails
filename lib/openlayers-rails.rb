2b36c5ef75a4a83f201f7a70a89713cf164fe0ea
Toms Mikoss <toms.mikoss@gmail.com>
2013-01-30 21:47:21 +0200

    Make sure the default theme is precompiled.

--- lib/openlayers-rails.rb
+++ lib/openlayers-rails.rb
@@ -3,6 +3,9 @@ require "openlayers-rails/version"
 module Openlayers
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'openlayers-rails' do |app|
+        app.config.assets.precompile += %w( openlayers/theme/default/style.css )
+      end
     end
   end
 end
